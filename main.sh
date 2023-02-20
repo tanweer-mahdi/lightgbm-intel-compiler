d56a93471bba9229b5376191f3b8378b310c744f
Shah Mahdi Hasan <shahmahdi.hasan@uon.edu.au>
2023-02-20 23:44:10 +1100

    Update main.sh
    Added lightgbm installation using non-binary mode

--- main.sh
+++ main.sh
@@ -3,3 +3,4 @@ sh ./l_BaseKit_p_2022.2.0.262_offline.sh -a --silent --eula accept
 source /opt/intel/oneapi/setvars.sh
 cmake -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx ..
 make -j4
+pip install --no-binary :all: lightgbm
