1409369abab41f7dcf377992d105f6f2d156a15e
Shah Mahdi Hasan <shahmahdi.hasan@uon.edu.au>
2023-02-20 23:42:43 +1100

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,3 @@
+wget https://registrationcenter-download.intel.com/akdlm/irc_nas/18673/l_BaseKit_p_2022.2.0.262_offline.sh
+sh ./l_BaseKit_p_2022.2.0.262_offline.sh -a --silent --eula accept
+source /opt/intel/oneapi/setvars.sh
