c1cb114045775a4677d8464b1f0d31db36bcac3f
Shah Mahdi Hasan <shahmahdi.hasan@uon.edu.au>
2023-02-20 23:43:16 +1100

    Update main.sh
    Added cmake with flags

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
 wget https://registrationcenter-download.intel.com/akdlm/irc_nas/18673/l_BaseKit_p_2022.2.0.262_offline.sh
 sh ./l_BaseKit_p_2022.2.0.262_offline.sh -a --silent --eula accept
 source /opt/intel/oneapi/setvars.sh
+cmake -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx ..
+make -j4
