97b99e396b055613d367c11072e9010325a579ec
Shah Mahdi Hasan <shahmahdi.hasan@uon.edu.au>
2023-04-15 23:49:28 +1000

    Added "wget" install command

--- main.sh
+++ main.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+apt-get install wget
 wget https://registrationcenter-download.intel.com/akdlm/irc_nas/18673/l_BaseKit_p_2022.2.0.262_offline.sh
 sh ./l_BaseKit_p_2022.2.0.262_offline.sh -a --silent --eula accept
 source /opt/intel/oneapi/setvars.sh
+cd /home/LightGBM/build
 cmake -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx ..
 make -j4
 pip install --no-binary :all: lightgbm
